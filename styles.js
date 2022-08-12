9c35268e59c8d3a88615ed8af1d2d9a762a2e27c
Nicolas Cisterna <43551684+junder79@users.noreply.github.com>
2022-08-12 15:14:14 -0400

    base proyecto

---
+++ styles.js
@@ -0,0 +1,49 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 40,
+    paddingHorizontal: 20,
+  },
+  centerText: {
+    flex: 1,
+    fontSize: 18,
+    padding: 32,
+    color: '#777',
+  },
+  textBold: {
+    fontWeight: '500',
+    color: '#000',
+  },
+  buttonText: {
+    fontSize: 21,
+    color: 'rgb(0,122,255)',
+  },
+  buttonTouchable: {
+    padding: 16,
+  },
+  containerList: {flex: 1, flexDirection: 'column'},
+  bluetoothStatusContainer: {justifyContent: 'flex-end', alignSelf: 'flex-end'},
+  bluetoothStatus: color => ({
+    backgroundColor: color,
+    padding: 8,
+    borderRadius: 2,
+    color: 'white',
+    paddingHorizontal: 14,
+    marginBottom: 20,
+  }),
+  bluetoothInfo: {
+    textAlign: 'center',
+    fontSize: 16,
+    color: '#FFC806',
+    marginBottom: 20,
+  },
+  sectionTitle: {fontWeight: 'bold', fontSize: 18, marginBottom: 12},
+  printerInfo: {
+    textAlign: 'center',
+    fontSize: 16,
+    color: '#E9493F',
+    marginBottom: 20,
+  },
+});
