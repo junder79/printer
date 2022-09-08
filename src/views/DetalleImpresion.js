c37b54fb4bcced365f09f9251b7be5e6d0091358
Nicolas Cisterna <43551684+junder79@users.noreply.github.com>
2022-09-08 15:29:35 -0400

    instalacion de react native paper

--- src/views/DetalleImpresion.js
+++ src/views/DetalleImpresion.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import {Text} from 'react-native';
+import {Avatar, Card, IconButton} from 'react-native-paper';
 const DetalleImpresion = () => {
-  return <Text>Detalle Impresion</Text>;
+  return (
+    <Card.Title
+      title="Card Title"
+      subtitle="Card Subtitle"
+      left={props => <Avatar.Icon {...props} icon="folder" />}
+      right={props => (
+        <IconButton {...props} icon="more-vert" onPress={() => {}} />
+      )}
+    />
+  );
 };
 
 export default DetalleImpresion;
